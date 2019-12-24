dfa03ee09ab39c8b4e51de3d93c71b3115f10c45
Nomadic <n.akylbekov@gmail.com>
2019-12-24 21:26:34 +0600

    GPS coordinate added, getting JSON parametrs from DarkSky is still unavailable

--- DarkSkyWeather/DarkSkyWeather/ViewController.swift
+++ DarkSkyWeather/DarkSkyWeather/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import CoreLocation
+import SwiftyJSON
+import Alamofire
 
-class ViewController: UIViewController
+class ViewController: UIViewController, CLLocationManagerDelegate
 {
     // UI Lables
     
@@ -16,11 +19,67 @@ class ViewController: UIViewController
     @IBOutlet weak var weatherDescriptionLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
     
-    override func viewDidLoad() {
+    let locationManager = CLLocationManager();
+    
+    // url addres and key given by darksky website
+    let weatherURL = ""
+    let key = ""
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        // Location
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
-
-
+    
+    
+    // Get GPS Coordinate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0
+        {
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            
+            print("longtitude = \(location.coordinate.longitude), latitude \(location.coordinate.latitude)")
+            
+            let lat = String(location.coordinate.latitude)
+            let lon = String(location.coordinate.longitude)
+            
+            
+            let params : [String : String] = ["key" : key, "latitude": lat, "longitude" : lon]
+            
+            getWeatherData(url: weatherURL, parametr: params)
+        }
+    }
+    
+    // If can't find location [no internet connection or airplane mode]
+     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+        print(error)
+    }
+    
+    // Get Weather from DarkSky Website
+    func getWeatherData(url: String, parametr: [String : String])
+    {
+        Alamofire.request(url, method: .get, parameters: parametr).responseJSON
+            {
+                response in
+                if response.result.isSuccess
+                {
+                    print("Success")
+                }
+                else
+                {
+                    print("Error")
+                }
+        }
+        
+    }
 }
 
