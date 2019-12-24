b46aa7703b331e1debf728e12e664b613810355e
Nomadic <n.akylbekov@gmail.com>
2019-12-24 19:34:08 +0600

    added labels""
     with '#' will be ignored, and an empty message aborts the commit.

--- DarkSkyWeather/DarkSkyWeather/ViewController.swift
+++ DarkSkyWeather/DarkSkyWeather/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+    // UI Lables
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
